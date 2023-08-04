2b2cae11248772d61958a26efe1431c1e5eb28d0
tu_nombre_de_usuario <maurotapiadev@gmail.com>
2023-08-04 18:33:27 -0300

    segundo commit

--- vite-c3/src/App.jsx
+++ vite-c3/src/App.jsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
+import './App.css'
+import NavBar from './Components/NavBar'
+
 
 function App() {
 
-  const ejemplo = <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, iure.</p>
-  
-  const [num, setNum] = useState(1);
-
-  const sumar = () => {
-    setNum(num+1);
-  } 
 
   return (
-    <div className='App'>
-      <h1>Hola mundo</h1>
-      <button onClick={sumar}>+1</button>
-      <p>{num}</p>
-      {ejemplo}
-    </div> 
-)
+    <>
+    <NavBar/>
+    </>
+  )
 }
 
 export default App
